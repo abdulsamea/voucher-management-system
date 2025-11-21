519c32d38bccafe984d51d3cc5539b497a2e0027
Abdul Samea <46262404+abdulsamea@users.noreply.github.com>
2025-11-21 19:51:10 +0530

    initial boilerplate added for voucher module (via Nest.js CLI)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VoucherModule } from './voucher/voucher.module';
 
 @Module({
-  imports: [],
+  imports: [VoucherModule],
   controllers: [AppController],
   providers: [AppService],
 })
